deaab498f722485e536445bf5fccbaeaf0074734
Tomsa Luca <lucasebastian777@gmail.com>
2025-12-19 17:01:55 +0200

    refactor 2025 classic level 1

--- 2025/classic-spaceship/level3/level3.cpp
+++ 2025/classic-spaceship/level3/level3.cpp
@@ -7,20 +7,21 @@ using namespace std;
 int main() {
     ifstream fin;
     ofstream fout;
-    fin.open("level1_2_large.in");
+    fin.open("level3_0_example.in");
     fout.open("output.out");
 
     string line;
-
     getline(fin, line);
+    const int paces[] = {-1, -2, -3, -4, -5, 0, 5, 4, 3, 2, 1};
+
     while (getline(fin, line)) {
         stringstream ss(line);
-        long long sum = 0;
-        long long num;
-        while (ss >> num) {
-            sum += num;
-        }
-        fout << sum << '\n';
+        int position, timeLimit;
+        ss >> position >> timeLimit;
+
+        fout << 0 << ' ';
+        
+        
     }
 
     return 0;
